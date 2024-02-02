5a078ad38d25f03eb3d10030d3ed60af3d8b0900
Yusuf <yhajiali7@gmail.com>
2024-02-02 15:50:52 +0000

    Add grid size and color inputs
    To be able to change the range of color and size

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const grid = document.querySelector(".grid");
+const gridSize = document.getElementById("grid-size");
+const color = document.getElementById("color");
+let gridSizeValue = 16;
 
-for (let i = 0; i < 256; i++) {
-  const gridItem = document.createElement("div");
-  gridItem.className = "grid-item";
-  grid.appendChild(gridItem);
-  console.log(i);
+// gridSize.addEventListener("input", () => {
+//   gridSizeValue = gridSize.value;
+// });
+
+color.addEventListener("input", () => {
+  colorChange(color.value);
+});
+
+for (let i = 0; i < gridSizeValue ** 2; i++) {
+  const div = document.createElement("div");
+  div.className = "grid-item";
+  grid.appendChild(div);
 }
+
+const gridItem = document.querySelectorAll(".grid-item");
+
+const colorChange = (color) => {
+  gridItem.forEach((item) =>
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = color;
+    })
+  );
+};
+
+colorChange(color.value);
