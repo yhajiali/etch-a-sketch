f3671aa66b3c9cc96e5a9822e3b79fb68e88a1a2
Yusuf <yhajiali7@gmail.com>
2024-02-03 10:56:21 +0000

    Fix grid item color highlight on hover
    Initialize grid-item then set value after creating and appending the items to the grid

--- script.js
+++ script.js
@@ -2,9 +2,31 @@ const grid = document.querySelector(".grid");
 const gridSize = document.getElementById("grid-size");
 const color = document.getElementById("color");
 const clearBtn = document.getElementById("clear");
+let gridItem;
+
+gridSize.addEventListener("input", () => {
+  gridSizeValue = gridSize.value;
+  setGridSize(gridSizeValue);
+});
+
+const setGridSize = (gridSizeValue = 16) => {
+  grid.innerHTML = ""; //Reset the grid by removing existing div items
+
+  // Re-add div items to the grid
+  for (let i = 0; i < gridSizeValue ** 2; i++) {
+    const div = document.createElement("div");
+    div.className = "grid-item";
+    div.style.width = `calc(100% / ${gridSizeValue})`; //set each width according to the grid size
+    grid.appendChild(div);
+  }
+
+  gridItem = document.querySelectorAll(".grid-item");
+};
+
+setGridSize();
 
 color.addEventListener("input", () => {
-  colorChange(color.value);
+  setColor(color.value);
 });
 
 clearBtn.addEventListener("click", () => {
@@ -13,25 +35,9 @@ clearBtn.addEventListener("click", () => {
   });
 });
 
-gridSize.addEventListener("input", () => {
-  gridSizeValue = gridSize.value;
-  changeGridSize(gridSizeValue);
-});
-
-const changeGridSize = (gridSizeValue = 16) => {
-  grid.innerHTML = "";
-
-  for (let i = 0; i < gridSizeValue ** 2; i++) {
-    const div = document.createElement("div");
-    div.className = "grid-item";
-    div.style.width = `calc(100% / ${gridSizeValue})`;
-    grid.appendChild(div);
-  }
-};
-
-const gridItem = document.querySelectorAll(".grid-item");
-
-const colorChange = (color) => {
+const setColor = (color) => {
+  console.log(color);
+  console.log(gridItem);
   gridItem.forEach((item) =>
     item.addEventListener("mouseover", () => {
       item.style.backgroundColor = color;
@@ -39,5 +45,4 @@ const colorChange = (color) => {
   );
 };
 
-colorChange(color.value);
-changeGridSize();
+setColor(color.value);
